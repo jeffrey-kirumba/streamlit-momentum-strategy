0abd6ebca42528ba96ee7d2c7cbe18a53e21d0bf
jeffrey-kirumba <jeffrey.kirumba@gmail.com>
2026-08-15 21:54:50 -0500

    comments, minor refactoring

--- momentum_strategy.py
+++ momentum_strategy.py
@@ -10,6 +10,7 @@ from statistics import mean
 import yfinance as yf
 import datetime
 import time
+from typing import List
 from concurrent.futures import ThreadPoolExecutor
         
 class Momo:
@@ -30,71 +31,93 @@ class Momo:
                     ]
         self.mainFrame = pd.DataFrame(columns = self.columns)
         self.closeData = {}
+    
+    def chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        final = []
+        for i in range(0, len(lst), n):
+            final.append(lst[i:i + n])
+        return final
 
-    def getCloseData(self, ticker):
-        symbObj = yf.Ticker(ticker)
-        data = symbObj.history(period="1y")
-        allCloseData = list(reversed(data['Close'].tolist()))
-        self.closeData[ticker] = allCloseData 
-
+    def getCloseData(self, tickers: List[str]):
+         tickData = yf.download(tickers=tickers, period="1y")
+         for tick in tickers:
+            allCloseData = list(reversed(tickData.loc[:, ('Close', tick)].tolist()))
+            self.closeData[tick] = allCloseData
+ 
     def getAllCloseData(self):
         stocks = pd.read_csv('sp_500_stocks.csv')
         allTickers = list(stocks['Ticker'])
-        with ThreadPoolExecutor() as executor:
-              executor.map(self.getCloseData, allTickers)
+        groups = len(allTickers) // 10
+        args = self.chunks(list(allTickers), groups)
+        for arg in args:
+            self.getCloseData(tickers=arg)
 
-    def getData(self): #speed this up w/ batch queries and threading
+    def getData(self):
         self.getAllCloseData()
         for symbol in self.closeData.keys():
             allData = self.closeData[symbol]
-            if len(allData) > 0:
+            if len(allData) >= 250 :
                 #year percent change
-                yearPercentChange =  allData[0] -  allData[-1] /allData[-1]
+                yearPercentChange =  abs(allData[0] -  allData[-1]) /allData[-1]
                 #6month percent
-                sixMonthPercentChange = allData[0] - allData[180] / allData[180]
+                sixMonthElement = len(allData) // 2
+                sixMonthPercentChange = abs(allData[0] - allData[sixMonthElement])/ allData[sixMonthElement]
                 #3month percent
-                threeMonthPercentChange = allData[0] - allData[90] / allData[90]
+                threeMonthElement = len(allData) // 4
+                threeMonthPercentChange = abs(allData[0] - allData[threeMonthElement])/ allData[threeMonthElement]
                 #1month percent
-                oneMonthPercentChange = allData[0] - allData[30] / allData[30]
+                oneMonthElement = len(allData) // 12
+                oneMonthPercentChange = abs(allData[0] - allData[oneMonthElement])/ allData[oneMonthElement]
                 series =  pd.Series([symbol, 
                             allData[0],
-                                                                'N/A',
+                            'N/A',
                             yearPercentChange,
-                                                                'N/A',
+                            'N/A',
                             sixMonthPercentChange,
-                                                                'N/A',
+                            'N/A',
                             threeMonthPercentChange,
-                                                                'N/A',
+                            'N/A',
                             oneMonthPercentChange,
-                                                                'N/A',
-                                                                'N/A'
-                                ], 
+                            'N/A',
+                            'N/A'], 
                                 index = self.columns)
                 self.mainFrame.loc[-1] = series
                 self.mainFrame.index+=1
                 self.mainFrame.sort_index()
+
+       #remove nan values
+        for row in self.mainFrame.index:
+            price = self.mainFrame.loc[row, 'Price']
+            if math.isnan(price):
+                self.mainFrame = self.mainFrame.drop([row], axis=0)
+
         time_periods = ['One-Year', 'Six-Month', 'Three-Month', 'One-Month']
+        #remove nan or null return vals
         for row in self.mainFrame.index:
                 for time_period in time_periods:
-                        if self.mainFrame.loc[row, f'{time_period} Price Return'] == None:
-                            self.mainFrame.loc[row, f'{time_period} Price Return'] = 0
+                    if self.mainFrame.loc[row, f'{time_period} Price Return'] == None or math.isnan(self.mainFrame.loc[row, f'{time_period} Price Return']):
+                        self.mainFrame.loc[row, f'{time_period} Price Return'] = 0
 
-                for row in self.mainFrame.index:
-                    for time_period in time_periods:
-                        self.mainFrame.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(self.mainFrame[f'{time_period} Price Return'], self.mainFrame.loc[row, f'{time_period} Price Return'])/100
+        #calc percentiles
+        for row in self.mainFrame.index:
+            for time_period in time_periods:
+                self.mainFrame.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(self.mainFrame[f'{time_period} Price Return'], self.mainFrame.loc[row, f'{time_period} Price Return'])/100
+        
+        #calc, then sort by HQM Score
+        for row in self.mainFrame.index:
+                momentum_percentiles = []
+                for time_period in time_periods:
+                    momentum_percentiles.append(self.mainFrame.loc[row, f'{time_period} Return Percentile'])
+                self.mainFrame.loc[row, 'HQM Score'] = mean(momentum_percentiles)
+        self.mainFrame = self.mainFrame.sort_values(by = 'HQM Score', ascending = False)
+        self.mainFrame =  self.mainFrame[:50]
+        self.mainFrame.reset_index(drop = True, inplace = True)
+                        
         print('# of Tickers collected after', len(self.mainFrame.index))
          
     def applyPortfolioValue(self, capital):
-        time_periods = ['One-Year', 'Six-Month', 'Three-Month', 'One-Month']
         mainFrame = self.mainFrame
-        for row in mainFrame.index:
-                momentum_percentiles = []
-                for time_period in time_periods:
-                    momentum_percentiles.append(mainFrame.loc[row, f'{time_period} Return Percentile'])
-                mainFrame.loc[row, 'HQM Score'] = mean(momentum_percentiles)
-        mainFrame = mainFrame.sort_values(by = 'HQM Score', ascending = False)
-        mainFrame =  mainFrame[:50]
-        mainFrame.reset_index(drop = True, inplace = True)
         position_size = float(capital) / len(mainFrame.index)
         for i in range(0, len(mainFrame['Ticker'])-1):
             try:
@@ -103,7 +126,15 @@ class Momo:
                  print(f'This was missing something: {e}')
         hqm_dataframe = mainFrame.replace(['N/A'], 0)
         return hqm_dataframe
+    
+@st.cache_resource(show_spinner=False)
+def buttonPushed():
+    with st.spinner('Gathering data'):
+        m.getData()
+        st.session_state.displayFrame = m.mainFrame
+    st.session_state.buttonPushed = True
 
+          
 m = Momo()
 displayFrame = None
 st.title('Quantitative Momentum Strategy')
@@ -111,15 +142,18 @@ st.write(""" ### This investing strategy selects 50 stocks from the S&P 500 with
 st.write(""" ### From there, it will recommended trades for an equal-weight portfolio of these 50 stocks.""")
 image = Image.open('momentum.jpg')
 st.image(image, use_column_width=True)
-capital = st.number_input('Enter the value of your portfolio')
-if 'displayFrame' not in st.session_state:
-    with st.spinner('Gathering data'):
-        m.getData()
-        st.session_state.displayFrame = m.mainFrame
-        displayFrame = m.mainFrame
-elif capital > 0:
-    m.mainFrame = st.session_state.displayFrame
-    displayFrame = m.applyPortfolioValue(capital=capital)
-st.table(displayFrame)
+
+
+displayFrame = None
+if 'buttonPushed' not in st.session_state:
+    st.button(label='Get Started', on_click=buttonPushed)
+
+if 'displayFrame' in st.session_state and not st.session_state.displayFrame.empty:
+    displayFrame = st.session_state.displayFrame
+    capital = st.number_input('Enter the value of your portfolio')
+    if capital > 0:
+        m.mainFrame = st.session_state.displayFrame
+        displayFrame = m.applyPortfolioValue(capital=capital)
+    st.table(displayFrame)
 
 
